dc7570f320ff27ee7c706ebc641e6dd00b90b28c
jacksonarrey <agborarrey93@gmail.com>
2023-07-22 10:22:38 +0000

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {});
+  const promise = new Promise((resolve) => {
+    resolve();
+  });
+
+  return promise;
 }
